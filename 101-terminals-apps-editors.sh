875fd73eb318b8a72a712c05e57c936856f513c7
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-08-21 19:45:33 -0500

    Create 101-terminals-apps-editors.sh

---
+++ 101-terminals-apps-editors.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# You can comment out any of these with a "#" and it won't install it.
+
+#####################################
+#	Terminals & Text Editors
+#####################################
+
+sudo apt install termite -yy
+
+sudo apt install rxvt-unicode urxvt-perls -yy
+
+sudo apt install ranger -yy
+
+sudo apt install feh -yy
+
+sudo apt install neofetch -yy
+
+sudo apt install vim -yy
+
+sudo apt install nano -yy
+
+sudo apt install geany -yy
+
+sudo apt install build-essential -yy
+
+sudo apt install cmake -yy
+
+sudo apt install p7zip p7zip-full unrar-free unzip -yy
+
+sudo apt install lshw -yy
+
