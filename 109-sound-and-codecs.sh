db5683066309fddd3bd37ef121c3c4d2c12120b9
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-08-21 19:51:14 -0500

    Create 109-sound-and-codecs.sh

---
+++ 109-sound-and-codecs.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+####################################
+#	Sound Stuff
+####################################
+
+sudo apt install pavucontrol -yy
+
+sudo apt install pulseaudio -yy
+
+sudo apt install pulseaudio-utils  -yy
+
+sudo apt install apulse -yy
+
+sudo apt install pulseaudio-dlna  -yy
+
+sudo apt install pulseaudio-equalizer -yy
+
+sudo apt install gstreamer1.0-pulseaudio -yy
+
+sudo apt install alsa-base -yy
+
+sudo apt install alsa-utils -yy
+
+sudo apt install gstreamer1.0-alsa -yy
+
+sudo apt install libao-common -yy
+
+sudo apt install libao-dbg -yy
+
+sudo apt install libao-dev -yy
+
+sudo apt intstall libao4 -yy
+
+sudo apt install libasound2 -yy
+
+sudo apt install libasound2-data  -yy
+
+sudo apt install libasound-dev -yy
+
+sudo apt install libasound-doc -yy
+
+sudo apt install libasound-plugins -yy 
+
+sudo apt install linux-sound-base -yy
+
+sudo apt install alsaplayer-gtk -yy
+
+sudo apt install alsaplayer-common -yy
+
+sudo apt install alsaplayer-alsa -yy
+
+sudo apt install alsaplayer-daemon -yy
+
+sudo apt install alsamixergui -yy
+
+sudo apt install volumeicon-alsa -yy
+
+sudo apt install ubuntu-restricted-extras -yy
+
+sudo apt install exfat-utils -yy
+
+
+
+
+
+
+
+
