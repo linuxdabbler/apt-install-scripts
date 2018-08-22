23cad9427bff60dccd3464b07f01a0a30097a306
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-08-21 19:59:16 -0500

    Create 112-desktop.sh
    this is stuff i find myself installing sometime along the way... I might as well include it.

---
+++ 112-desktop.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#####################################
+#	Desktop
+#####################################
+
+sudo apt install synaptic -yy
+
+sudo apt install thunar -yy
+
+sudo apt install thunar-dropbox-plugin -yy
+
+sudo apt install thunar-media-tags-plugin -yy
+
+sudo apt install thunar-vcs-plugin -yy
+
+sudo apt install thunar-volman -yy
+
+sudo apt install ffmpegthumbnailer -yy
+
+sudo apt install cups cups-pdf -yy
+
+sudo apt install variety -yy
+
+sudo apt install nitrogen -yy
+
+sudo apt install lxappearance -yy
+
+sudo apt install dmenu -yy
+
+sudo apt install rofi -yy
