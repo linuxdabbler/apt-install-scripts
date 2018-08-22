078cfe042741ec0f815fefd4c9ea42219eca046a
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-08-21 19:46:06 -0500

    Create 102-downloaders.sh

---
+++ 102-downloaders.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+####################################
+#	Downloaders & Stuff
+####################################
+
+sudo apt install transmission-gtk -yy
+
+sudo apt intall git -yy
+
+sudo apt install wget -yy
+
+sudo apt install gdebi -yy
+
+
