7a4764942483b8d48c824b0c28b4ae78ee5351fd
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-08-21 19:48:57 -0500

    Create 106-web-browsers.sh

---
+++ 106-web-browsers.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+####################################
+#	Web Browsers
+####################################
+
+sudo apt install firefox -yy
+
+sudo apt install chromium-browser -yy
+
+sudo apt install chromium-chromedriver -yy
