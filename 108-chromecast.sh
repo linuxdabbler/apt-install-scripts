c04434ecaab8b5dbd64a34b95897f836dd182da1
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-08-21 19:50:25 -0500

    Create 108-chromecast.sh

---
+++ 108-chromecast.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+##############################################
+#	Chromecast Stuff
+##############################################
+
+sudo apt install mkchromecast -yy
+
+sudo apt install mkchromecast-alsa -yy
+
+sudo apt install mkchromecast-gstreamer -yy
+
+sudo apt install mkchromecast-pulseaudio -yy
+
+sudo apt install pulseaudio-dlna -yy
+
+sudo apt install python-pychromecast -yy
