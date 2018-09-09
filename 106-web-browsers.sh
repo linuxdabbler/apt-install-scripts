e5be7c19f14cab3982edca90fe15312f4381ea6b
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-09-09 09:30:31 -0500

    Update 106-web-browsers.sh

--- 106-web-browsers.sh
+++ 106-web-browsers.sh
@@ -4,8 +4,21 @@
 #	Web Browsers
 ####################################
 
+# Install firefox
 sudo apt install firefox -yy
 
+# Install chromium
 sudo apt install chromium-browser -yy
-
 sudo apt install chromium-chromedriver -yy
+
+# cd /tmp to download and build package
+cd /tmp
+
+# Install google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome*.deb
+
+# cd back to the script folder
+cd ~/apt-install-scripts
+
