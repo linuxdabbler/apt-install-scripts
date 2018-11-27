033767a0c62b779217a043ca9dd35c3bc5126cb2
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-11-27 09:48:51 -0600

    Update 099-move-wallapers.sh

--- 099-move-wallapers.sh
+++ 099-move-wallapers.sh
@@ -2,20 +2,23 @@
 # cd into the scripts folder
 cd apt-install-scripts
 
+sudo mv *.png /usr/share/backgrounds
+sudo mv *.jpg /usr/share/backgrounds
+
 # make a folder in the Pictures folder called LDWP
-mkdir ~/Pictures/LDWP
+#mkdir ~/Pictures/LDWP
 
 # move all .PNG images to LDWP
-sudo mv *.png ~/Pictures/LDWP/
+#sudo mv *.png ~/Pictures/LDWP/
 
 # cd into LDWP (where we just sent the pictures)
-cd ~/Pictures/LDWP
+#cd ~/Pictures/LDWP
 
 
 # copy all proper background images to the backgrounds directory without the templates (they are still available to edit)
-sudo cp LDWP* /usr/share/backgrounds
+#sudo cp LDWP* /usr/share/backgrounds
 
 # cd back into the script folder to run the other scripts 
-cd ~/apt-install-scripts
+#cd ~/apt-install-scripts
 
 exit
