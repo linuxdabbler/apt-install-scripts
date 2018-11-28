f5c4e318fec05f0ce3bd390f645fef8e62d540bb
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-11-28 08:58:01 -0600

    Update 103-firewall-and-utilities.sh

--- 103-firewall-and-utilities.sh
+++ 103-firewall-and-utilities.sh
@@ -14,6 +14,14 @@ sudo apt install bleachbit -yy
 
 sudo apt install ukuu -yy
 
+# Install Etcher AppImage
+
+sudo apt install gdebi
+
+wget https://github.com/resin-io/etcher/releases/download/v1.1.1/etcher-electron_1.1.1_amd64.deb
+
+sudo gdebi etcher-electron_1.1.1_amd64.deb
+
 # Download the PIA installer
 
 wget https://www.privateinternetaccess.com/installer/download_installer_linux
