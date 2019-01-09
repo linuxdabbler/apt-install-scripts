4515f8378cb42d4b53df1c5bf68b968becf713d6
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-01-09 14:14:35 -0600

    Update and rename 106-web-browsers.sh to 106-firefox-chromium.sh

---
+++ 106-firefox-chromium.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+####################################
+#	Web Browsers
+####################################
+
+# Install firefox
+sudo apt install firefox -yy
+
+# Install chromium
+sudo apt install chromium-browser -yy
+sudo apt install chromium-chromedriver -yy
+
+
