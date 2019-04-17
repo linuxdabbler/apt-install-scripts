95c195e65c543da8a2970b272dce0fde0ed8a7ef
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-04-16 22:29:36 -0500

    Create 100-tasksel.sh

---
+++ 100-tasksel.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo apt install tasksel tasksel-data astro-tasks blends-tasks debichem-tasks electronics-tasks games-tasks gis-tasks hamradio-tasks junior-tasks med-tasks multimedia-tasks science-tasks
+
